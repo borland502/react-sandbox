736ce5e2206b248567066db7cee5505972bb9a41
Jeremy Hettenhouser <jeremy.hettenhouser@icf.com>
2025-05-19 13:49:56 -0400

    Chapter 2

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,90 @@ const fibonacci = (n: number): number => {
 function renderFibonacci() {
 	const fibNumbers = fibIndex.map((idx) => fibonacci(idx));
 	return fibNumbers.map((num, idx) => (
-		<div key={idx}>
+		<li key={idx}>
 			Fibonacci index {fibIndex[idx]} is {num}
+		</li>
+	));
+}
+
+const titleList = [
+	{
+		objectId: 1,
+		title: "React",
+		url: "https://reactjs.org/",
+		author: "Meta",
+		num_comments: 0,
+		points: 0,
+	},
+	{
+		objectId: 2,
+		title: "Angular",
+		url: "https://angular.io/",
+		author: "Google",
+		num_comments: 0,
+		points: 0,
+	},
+	{
+		objectId: 3,
+		title: "Vue",
+		url: "https://vuejs.org/",
+		author: "Evan You",
+		num_comments: 0,
+		points: 0,
+	},
+	{
+		objectId: 4,
+		title: "Svelte",
+		url: "https://svelte.dev/",
+		author: "Rich Harris",
+		num_comments: 0,
+		points: 0,
+	},
+	{
+		objectId: 5,
+		title: "Ember",
+		url: "https://emberjs.com/",
+		author: "Yehuda Katz",
+		num_comments: 0,
+		points: 0,
+	},
+	{
+		objectId: 6,
+		title: "Backbone",
+		url: "https://backbonejs.org/",
+		author: "Jeremy Ashkenas",
+		num_comments: 0,
+		points: 0,
+	},
+	{
+		objectId: 7,
+		title: "Preact",
+		url: "https://preactjs.com/",
+		author: "Jason Miller",
+		num_comments: 0,
+		points: 0,
+	},
+	{
+		objectId: 8,
+		title: "Inferno",
+		url: "https://infernojs.org/",
+		author: "Dominic Tarr",
+		num_comments: 0,
+		points: 0,
+	},
+];
+
+function renderTitleList() {
+	return titleList.map((item) => (
+		<div key={item.objectId}>
+			<ul>
+				<li>
+					<a href={item.url}>{item.title}</a>
+					<span> by {item.author}</span>
+					<span> | {item.num_comments} comments</span>
+					<span> | {item.points} points</span>
+				</li>
+			</ul>
 		</div>
 	));
 }
@@ -25,11 +107,15 @@ function App() {
 	return (
 		<div>
 			<h1>
-				{renderFibonacci()}
 				{welcome.greeting} {welcome.title}
 			</h1>
-			<label htmlFor="search">Search:</label>
+			<label htmlFor="search">Search: </label>
 			<input type="text" id="search" />
+
+			<hr />
+
+			<ul>{renderFibonacci()}</ul>
+			<ul>{renderTitleList()}</ul>
 		</div>
 	);
 }
